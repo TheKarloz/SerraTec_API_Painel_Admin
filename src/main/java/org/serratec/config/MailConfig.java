cfa1b1d5d0013f690318db3b0702e7859f3708ea
Thiago_Marquet <thiagomarquet111@gmail.com>
2022-06-07 16:18:35 -0300

    correção MailConfig

--- src/main/java/org/serratec/config/MailConfig.java
+++ src/main/java/org/serratec/config/MailConfig.java
@@ -6,7 +6,6 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
@@ -17,19 +16,6 @@ public class MailConfig {
 	@Autowired
 	private JavaMailSender javaMailSender;
 		
-
-	public void enviarEmail(String para, String assunto, String nome, String texto) {
-		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-
-		simpleMailMessage.setFrom("grupo2.serratec.api@gmail.com");
-		simpleMailMessage.setTo(para);
-		simpleMailMessage.setSubject(assunto);
-		simpleMailMessage.setText("Olá, " + nome + " Obrigado por se cadastrar em nossa loja!\n\n\n"
-		+ texto +
-		"\n\nAtenciosamente\nLoja de Calçados");
-		javaMailSender.send(simpleMailMessage);
-	}
-
 	public void enviarEmailHTML(String para, String assunto,String texto) throws MessagingException{
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
